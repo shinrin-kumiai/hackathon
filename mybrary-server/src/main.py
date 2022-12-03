bcfae218af9f3e34350b7cb7581e72243534cbf8
DENCYUPC\Dency <takotakocyuman@gmail.com>
2022-12-03 19:04:00 +0900

    mainファイルとdependenciesファイルを作成しました

---
+++ mybrary-server/src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from src.dependencies import get_db
+from src.routers import users, communities, books
+
+
+app = FastAPI()
+app.include_router(users.router)
+app.include_router(communities.router)
+app.include_router(books.router)
+
+
+@app.get('/hello')
+async def index():
+    return "Hello world!"
