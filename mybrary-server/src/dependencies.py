ccccc1e867958d5b376f359d725468391dfcd25f
DENCYUPC\Dency <takotakocyuman@gmail.com>
2022-12-12 10:57:54 +0900

    書影の保存先パスを環境変数化＆Depends化しました

--- mybrary-server/src/dependencies.py
+++ mybrary-server/src/dependencies.py
@@ -5,4 +5,10 @@ def get_db() -> SessionLocal:
     try:
         yield db
     finally:
-        db.close()
+        db.close()def get_thumbnail_save_path() -> str:
+    """書影の保存先パスを返す関数
+
+    Returns:
+        str: .envから取得した書影の保存先パス
+    """
+    return os.environ.get("THUMBNAIL_SAVE_PATH")
