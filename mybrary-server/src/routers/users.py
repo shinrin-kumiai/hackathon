bd011865d105e3c56eae8f0c7f2c331b7a9a2b1a
DENCYUPC\Dency <takotakocyuman@gmail.com>
2022-12-12 10:59:17 +0900

    ユーザー所有本の登録機能を実装しました

--- mybrary-server/src/routers/users.py
+++ mybrary-server/src/routers/users.py
@@ -1,18 +1,51 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.orm import Session
 
-from src.dependencies import get_db
+from src.dependencies import get_db, get_current_user, get_thumbnail_save_path
+from src import crud, services
 
 router = APIRouter(
-    prefix='/user',
     tags=['user']
 )
 
 
-@router.get("/all")
-async def get_users(db: Session = Depends(get_db)) -> dict:
-    return {"message": "This end-point is in the user.py!"}
+@router.post("/user/books/register/")
+async def register_book(
+    isbn: str = Query(..., description = "登録したい本のisbnコード"),
+    db: Session = Depends(get_db),
+    user_id: str = Depends(get_current_user),
+    thumbnail_save_path: str = Depends(get_thumbnail_save_path)
+) -> None:
+    try:
+        isbn = services.isbn_normalize(isbn)
+        isbn = services.toggle_isbn10_and_isbn13(isbn) if len(isbn) == 10 else isbn
+        target_book = crud.search_book_by_isbn(db=db, isbn=isbn)
 
-@router.get("/{id}")
-async def get_users(db: Session = Depends(get_db)) -> dict:
-    return {"message": "This end-point is in the user.py!"}
+        if target_book is None:
+            xml_data = services.get_xml_data_from_ndl(isbn)
+            res_json = services.xml_to_json(xml_data=xml_data)
+            if res_json is None:
+                raise HTTPException(
+                    status_code=404,
+                    detail="指定されたisbnコードの本は見つかりませんでした."
+                )
+            services.get_thumbnail_from_ndl(
+                isbn=isbn,
+                thumbnail_save_path=thumbnail_save_path
+            )
+
+            crud.register_book(db=db, book_data_json=res_json)
+            target_book = crud.search_book_by_isbn(db=db, isbn=isbn)
+
+        crud.associate_book_to_user(
+            db=db,
+            user_id=user_id,
+            book_id=target_book.id
+        )
+        return {"message": "本が正常に登録されました."}
+
+    except HTTPException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.detail)
+    except:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
