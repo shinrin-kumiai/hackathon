fdb28ee4efa881490c09108c87dd25f07806354e
masa-tichita <bunmamasa@docomo.ne.jp>
2022-12-13 10:16:52 +0900

    db_connectを使用してengineを切り替えれるようにする

--- mybrary-server/src/database/database.py
+++ mybrary-server/src/database/database.py
@@ -1,11 +1,26 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from src.database.db_connect import azure_db_connect
+import dotenv
+dotenv.load_dotenv(override=True)
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///mybrary.sqlite3"
+if os.environ.get("IS_LOCAL_DB")=="true":
+    SQLALCHEMY_DATABASE_URL = "sqlite:///mybrary.sqlite3"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    ###Azureへの接続################################
+    server=os.environ.get("SERVER")
+    database=os.environ.get("DATABASE")
+    username=os.environ.get("USERNAME")
+    password=os.environ.get("PASSWORD")
+    
+    engine=azure_db_connect(server,database,username,password)
+    print(engine)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- mybrary-server/src/database/db_connect.py
+++ mybrary-server/src/database/db_connect.py
@@ -7,58 +7,68 @@ from sqlalchemy.orm import declarative_base,sessionmaker,relationship,Session
 dotenv.load_dotenv(override=True)
 
 
-if os.environ.get("IS_LOCAL_DB")=="true":
-    SQLALCHEMY_DATABASE_URL = "sqlite:///mybrary.sqlite3"
+# if os.environ.get("IS_LOCAL_DB")=="true":
+#     SQLALCHEMY_DATABASE_URL = "sqlite:///mybrary.sqlite3"
 
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
-else:
-    ###Azureへの接続################################
-    server=os.environ.get("SERVER")
-    database=os.environ.get("DATABASE")
-    username=os.environ.get("USERNAME")
-    password=os.environ.get("PASSWORD")
+#     engine = create_engine(
+#         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+#     )
+# else:
+#     ###Azureへの接続################################
+#     server=os.environ.get("SERVER")
+#     database=os.environ.get("DATABASE")
+#     username=os.environ.get("USERNAME")
+#     password=os.environ.get("PASSWORD")
+def azure_db_connect(server,database,username,password):
 
-odbc_connect=urllib.parse.quote_plus(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
-engine = create_engine('mssql+pyodbc:///?odbc_connect=' + odbc_connect)
+    odbc_connect=urllib.parse.quote_plus(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
+    engine = create_engine('mssql+pyodbc:///?odbc_connect=' + odbc_connect)
 
-with engine.connect() as conn:
-    #print(vars(conn.engine))
-    rs = conn.execute('SELECT @@VERSION as version')
-    for row in rs:
-        print(row['version'])
+    with engine.connect() as conn:
+        #print(vars(conn.engine))
+        rs = conn.execute('SELECT @@VERSION as version')
+        for row in rs:
+            print(row['version'])
 
-print(row['version'])
-print("接続に成功しました")
+    print(row['version'])
+    print("接続に成功しました")
+
+    return engine
+
+# if __name__ == "__main__":
+#     azure_db_connect(server=os.environ.get("SERVER"),
+#                     database=os.environ.get("DATABASE"),
+#                     username=os.environ.get("USERNAME"),
+#                     password=os.environ.get("PASSWORD"))
+
 ################################################
 
 ####testテーブルの作成#####
-Base=declarative_base()
-#生徒テーブル
-class Student(Base):
-    __tablename__ = "students_test"
-    id = Column(Integer,primary_key=True,autoincrement=True)
-    name = Column(String(255))
-    class_name=Column(String(14))
-    tall=Column(Integer)
-    weight=Column(Integer)
-    test=relationship("Test",back_populates="students")
+# Base=declarative_base()
+# #生徒テーブル
+# class Student(Base):
+#     __tablename__ = "students_test"
+#     id = Column(Integer,primary_key=True,autoincrement=True)
+#     name = Column(String(255))
+#     class_name=Column(String(14))
+#     tall=Column(Integer)
+#     weight=Column(Integer)
+#     test=relationship("Test",back_populates="students")
 
-#テーブル科目と点数テーブル
-class Test(Base):
-    __tablename__ = "test_subject_data"
-    id = Column(Integer,primary_key=True,autoincrement=True)
-    student_id=Column(Integer,ForeignKey("students_test.id"))
-    students=relationship("Student",back_populates="test")
-    test_score=Column(Integer)
-    test_subject=Column(String(255))
+# #テーブル科目と点数テーブル
+# class Test(Base):
+#     __tablename__ = "test_subject_data"
+#     id = Column(Integer,primary_key=True,autoincrement=True)
+#     student_id=Column(Integer,ForeignKey("students_test.id"))
+#     students=relationship("Student",back_populates="test")
+#     test_score=Column(Integer)
+#     test_subject=Column(String(255))
 
-print("DBにテーブルを追加")
+# print("DBにテーブルを追加")
 
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 #SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # session=Session()
@@ -73,17 +83,17 @@ Base.metadata.create_all(engine)
 
 # Session = sessionmaker(bind=engine)
 # session = Session()
-print(f"engineの中身を確認{engine}")
+# print(f"engineの中身を確認{engine}")
 
-with Session(bind=engine) as db:
-    add_student_data=(Student(
-                        name = "mori",
-                        class_name="2",
-                        tall=173,
-                        weight=60,))
+# with Session(bind=engine) as db:
+#     add_student_data=(Student(
+#                         name = "mori",
+#                         class_name="2",
+#                         tall=173,
+#                         weight=60,))
 
-    db.add(add_student_data)
-    db.commit()
+#     db.add(add_student_data)
+#     db.commit()
 
 
         #subjects=["math","english","science"]
