6ed30ecbc00726e7d8e3956080eecfa6522c1d45
DENCYUPC\Dency <takotakocyuman@gmail.com>
2022-12-13 12:59:30 +0900

    書影が見つからなかった場合にも登録できるようにしました

--- mybrary-server/tests/unit/test_users.py
+++ mybrary-server/tests/unit/test_users.py
@@ -76,7 +76,46 @@ def test_isbnコードが9784798167206である未登録の本の登録とユー
         res_json = response.json()
         assert response.status_code == 200
         assert res_json["message"] == "本が正常に登録されました."
-        assert glob.glob("/tests/tmp/9784798167206.jpg") is not None
+        assert len(glob.glob("/tests/tmp/9784798167206.jpg")) == 1
+
+        with Session(bind=engine) as db:
+            all_books = db.query(models.Book).all()
+            assert len(all_books) == 11
+            all_user1_books = db.query(models.UserBook)\
+                .filter(models.UserBook.user_id == "user0001-0000-0000-0000-000000000000")\
+                    .all()
+            assert len(all_user1_books) == 1
+    else:
+        pass
+
+
+def test_書影が登録されていない本のisbnコードである9784785300019を指定してPOSTし正常に登録される():
+    """正常形テスト(/user/books/register/)
+    1. テストDBにおいて本の数が10冊であることの確認
+    2. ユーザー1の所有している本の冊数が0冊であることの確認
+    3. テスト対象エンドポイントを叩き、レスポンスを取得
+    4. レスポンスのステータスコードとメッセージを確認
+    5. tests/tmp/9784798167206.jpgが保存されていないことの確認
+    6. テストDBにおいて本の冊数が11冊になっていることの確認
+    7. ユーザー1の所有している本の冊数が1冊になっていることの確認
+
+    Note:
+        .env > NDLAPI_RELATED_TEST_EXECUTE_IS == "true"の場合のみテスト実行
+    """
+    if os.environ.get('NDLAPI_RELATED_TEST_EXECUTE_IS') == "true":
+        with Session(bind=engine) as db:
+            all_books = db.query(models.Book).all()
+            assert len(all_books) == 10
+            all_user1_books = db.query(models.UserBook)\
+                .filter(models.UserBook.user_id == "user0001-0000-0000-0000-000000000000")\
+                    .all()
+            assert len(all_user1_books) == 0
+
+        response = client.post("/user/books/register/?isbn=9784785300019")
+        res_json = response.json()
+        assert response.status_code == 200
+        assert res_json["message"] == "本が正常に登録されました."
+        assert len(glob.glob("/tests/tmp/9784785300019.jpg")) == 0
 
         with Session(bind=engine) as db:
             all_books = db.query(models.Book).all()
@@ -102,23 +141,6 @@ def test_情報がNDLに登録されていない本のisbnコードである9784
         res_json = response.json()
         assert response.status_code == 404
         assert res_json["detail"] == "指定されたisbnコードの本は見つかりませんでした."
-    else:
-        pass
-
-
-def test_書影が登録されていない本のisbnコードである9784785300012を指定してPOSTし404エラーを吐く():
-    """異常系テスト(/user/books/register/)
-    1. NDLに書影が未登録の本のisbnコードである9784785300012を指定してPOST
-    2. レスポンスのステータスコードと詳細を確認
-
-    Note:
-        .env > NDLAPI_RELATED_TEST_EXECUTE_IS == "true"の場合のみテスト実行
-    """
-    if os.environ.get('NDLAPI_RELATED_TEST_EXECUTE_IS') == "true":
-        response = client.post("/user/books/register/?isbn=9784785300012")
-        res_json = response.json()
-        assert response.status_code == 404
-        assert res_json["detail"] == "書影が見つかりませんでした."
     else:
         pass
 
