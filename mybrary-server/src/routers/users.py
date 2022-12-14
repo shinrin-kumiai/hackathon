33317e3a6e164c6dbbd324cf38a606f128f543fc
DENCYUPC\Dency <takotakocyuman@gmail.com>
2022-12-14 14:39:46 +0900

    ユーザーの所属しているコミュニティの情報を配列で取得するエンドポイントの追加をしました

--- mybrary-server/src/routers/users.py
+++ mybrary-server/src/routers/users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends, Query
 from fastapi_pagination import Page, paginate
 from sqlalchemy.orm import Session
+from typing import List
 
 from src.dependencies import get_db, get_current_user, get_thumbnail_save_path
 from src import crud, services, schemas
@@ -95,6 +96,21 @@ async def delete_book_by_id(
             )
         crud.delete_user_book_by_id(db=db, book_id=book_id)
         return {"message": f"id:{book_id}の本を削除しました."}
+
+    except HTTPException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.detail)
+    except:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@router.get("/communities", response_model=List[schemas.CommunityInfo])
+async def get_belong_communities(
+    db: Session = Depends(get_db),
+    user_id: str = Depends(get_current_user)
+) -> str:
+    try:
+        belong_communities = crud.search_user_by_id(db=db, user_id=user_id).community
+        return list(map(schemas.CommunityInfo.mapping_to_dict, belong_communities))
 
     except HTTPException as e:
         raise HTTPException(status_code=e.status_code, detail=e.detail)
