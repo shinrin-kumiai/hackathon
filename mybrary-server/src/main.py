e4a728c6af3924155c89b30f7088afaa7cedc576
DENCYUPC\Dency <takotakocyuman@gmail.com>
2022-12-16 22:07:36 +0900

    認証のコードを追記しました

--- mybrary-server/src/main.py
+++ mybrary-server/src/main.py
@@ -3,10 +3,23 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 
 from src.routers import users, assets, communities
+from src.utils.config import settings
 
 
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url=f'{settings.API_V1_STR}/openapi.json',
+    swagger_ui_oauth2_redirect_url='/oauth2-redirect',
+    swagger_ui_init_oauth={
+        'usePkceWithAuthorizationCodeGrant': True,
+        'clientId': settings.OPENAPI_CLIENT_ID,
+        'additionalQueryStringParams': {'prompt': 'consent'},
+    },
+    version='1.0.0',
+    description='## Welcome to my API! \n This is my description, written in `markdown`',
+    title=settings.PROJECT_NAME,
+)
+
 app.include_router(users.router)
 app.include_router(assets.router)
 app.include_router(communities.router)
---
+++ mybrary-server/src/utils/config.py
@@ -0,0 +1,39 @@
+import os
+import dotenv
+from typing import List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, Field, HttpUrl
+
+dotenv.load_dotenv(override=True)
+
+
+class AzureActiveDirectory(BaseSettings):
+    OPENAPI_CLIENT_ID: str = Field(default='', env='OPENAPI_CLIENT_ID')
+    TENANT_ID: str = Field(default='', env='TENANT_ID')
+    APP_CLIENT_ID: str = Field(default='', env='APP_CLIENT_ID')
+    AUTH_URL: AnyHttpUrl = Field(default='https://dummy.com/', env='AUTH_URL')
+    CONFIG_URL: AnyHttpUrl = Field(default='https://dummy.com/', env='CONFIG_URL')
+    TOKEN_URL: AnyHttpUrl = Field(default='https://dummy.com/', env='TOKEN_URL')
+    GRAPH_SECRET: str = Field(default='', env='GRAPH_SECRET')
+    CLIENT_SECRET: str = Field(default='', env='CLIENT_SECRET')
+
+
+class Settings(AzureActiveDirectory):
+    API_V1_STR: str = '/api/v1'
+    SECRET_KEY: str = Field(..., env='SECRET_KEY')
+
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[Union[str, AnyHttpUrl]] = ['http://localhost:8000']
+
+    PROJECT_NAME: str = 'My Project'
+    SENTRY_DSN: Optional[HttpUrl] = None
+
+    class Config:  # noqa
+        env_file = 'demo_project/.env'
+        env_file_encoding = 'utf-8'
+        case_sensitive = True
+
+
+settings = Settings()
