2494b563afc993aaf3d0575836039593ebba9d19
masa-tichita <bunmamasa@docomo.ne.jp>
2022-12-19 10:40:30 +0900

    情報を更新しました

--- mybrary-front/src/apiConfig.js
+++ mybrary-front/src/apiConfig.js
@@ -1,4 +1,4 @@
 export const apiConfig = {
-  b2cScopes: ["https://hackathon.onmicrosoft.com/65fbc00d-f580-45f1-907d-cf72fda2371b/tasks.read"],
-  webApi: "http://localhost:5173"
+  b2cScopes: ["https://usehackathon.onmicrosoft.com/65fbc00d-f580-45f1-907d-cf72fda2371b/tasks.read"],
+  webApi: "http://localhost:8000"
 };
--- mybrary-front/src/authConfig.js
+++ mybrary-front/src/authConfig.js
@@ -6,7 +6,7 @@ export const msalConfig = {
     clientId: "f146ba92-5e30-473e-a164-36b2aa6559d8", // This is the ONLY mandatory field; everything else is optional.
     authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
     knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
-    redirectUri: "http://localhost:5173/login/redirect", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
+    redirectUri: "https://ashy-beach-0bd0cb400.2.azurestaticapps.net/login/redirect", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
   },
   cache: {
     cacheLocation: "sessionStorage",  
